26bf1771428cddac72767887b5ddd72146a0e385
larryvsdata <ermanbekaroglu@gmail.com>
2018-09-27 18:45:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,10 @@ katz_deli = []
 def line
   base_string="The line is currently "
   if katz_deli.size==0 
-    return base_string<<"empty"
+    return base_string<<"empty."
+  else
+  base_string= base_string<<":"
+  
     return 
   end
 end
